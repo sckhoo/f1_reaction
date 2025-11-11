8f3e02cb4271ef6005453398b0226ec7625e867b
SC Khoo <sckhoo@gmail.com>
2025-11-11 21:49:11 +0800

    add google font

--- lib/game.dart
+++ lib/game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Game extends StatelessWidget {
   const Game({super.key});
@@ -10,13 +11,26 @@ class Game extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Text('Game Screen - Under Construction 1'),
-            Text('Game Screen - Under Construction 2 - instructions here'),
+            Text(
+              'Game Screen - Under Construction 1',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Game Screen - Under Construction 2 - instructions here',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            ),
             SizedBox(height: 50),
             Expanded(child: SizedBox(child: Text('Game Area'))),
             Image.asset('assets/images/ferrari_cockpit.jpg', width: 300),
             SizedBox(height: 50),
-            Text('Tap back to return to home screen'),
+            Text(
+              'Tap back to return to home screen',
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ],
         ),
       ),
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
 import 'package:f1_reaction/game.dart';
 
 class MyApp extends StatelessWidget {
